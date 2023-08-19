3042109ff654342c55c355fe4431894442ede1eb
Anirudh Srivathsavan S <anirudhsrivathsavan@gmail.com>
2023-08-20 00:43:26 +0530

    fluid layouts
    added scalability and rectified the database

--- database/petwebsite.sql
+++ database/petwebsite.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 17, 2023 at 09:01 AM
+-- Generation Time: Aug 19, 2023 at 09:12 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -24,29 +24,79 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `animal`
+--
+
+CREATE TABLE `animal` (
+  `Number` int(11) NOT NULL,
+  `Animal` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `placedata`
 --
 
 CREATE TABLE `placedata` (
   `Name` varchar(30) NOT NULL COMMENT 'Non nullable',
-  `id` int(10) UNSIGNED NOT NULL COMMENT 'for indexing purpose',
+  `Id` int(10) UNSIGNED NOT NULL COMMENT 'for indexing purpose',
   `Address` varchar(100) NOT NULL COMMENT 'Contains address of service',
+  `Zip code` mediumint(8) UNSIGNED DEFAULT NULL,
   `Byline` varchar(100) NOT NULL,
   `Phone` varchar(20) DEFAULT NULL,
   `imgpath` varchar(100) DEFAULT NULL COMMENT 'img stored in ./images',
-  `Avail` varchar(100) DEFAULT NULL
+  `Avail` varchar(100) DEFAULT NULL,
+  `Service` int(11) NOT NULL,
+  `Animal` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `service`
+--
+
+CREATE TABLE `service` (
+  `Number` int(11) NOT NULL,
+  `Service` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `service`
+--
+
+INSERT INTO `service` (`Number`, `Service`) VALUES
+(1, 'Veterinarian'),
+(2, 'Breeder'),
+(3, 'Rescue'),
+(4, 'Shelter'),
+(5, 'Groomer'),
+(6, 'Foster'),
+(7, 'Animal Control');
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `animal`
+--
+ALTER TABLE `animal`
+  ADD PRIMARY KEY (`Number`);
+
+--
 -- Indexes for table `placedata`
 --
 ALTER TABLE `placedata`
-  ADD PRIMARY KEY (`id`,`Address`,`Byline`),
-  ADD UNIQUE KEY `id` (`id`,`Byline`);
+  ADD PRIMARY KEY (`Id`,`Address`,`Byline`),
+  ADD UNIQUE KEY `id` (`Id`,`Byline`);
+
+--
+-- Indexes for table `service`
+--
+ALTER TABLE `service`
+  ADD PRIMARY KEY (`Number`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -56,7 +106,7 @@ ALTER TABLE `placedata`
 -- AUTO_INCREMENT for table `placedata`
 --
 ALTER TABLE `placedata`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'for indexing purpose';
+  MODIFY `Id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'for indexing purpose';
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
