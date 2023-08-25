42eee18affd6d2d146d18775085fc25b9073795b
Anirudh Srivathsavan S <anirudhsrivathsavan@gmail.com>
2023-08-26 00:19:34 +0530

    JS completion

--- code/formvalidator.js
+++ code/formvalidator.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-
     const zipcodeInput = document.getElementById('zipcode');
     const zipcodeError = document.getElementById('zipcode-error');
     const submitButton = document.getElementById('submit-button');
@@ -17,3 +16,16 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const btn1 = document.getElementById('btn-1');
+    const btn2 = document.getElementById('btn-2');
+    let selectedButton = (btn1.checked)?btn1:btn2;
+
+    setInterval(function() {
+        selectedButton.checked = false;
+        selectedButton = (selectedButton === btn1) ? btn2 : btn1;
+        selectedButton.checked = true;
+    }, 5000);
+});
+
